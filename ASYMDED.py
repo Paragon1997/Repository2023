70db2c54897caf9a6c69c77afcfb4c27656745de
Paragon1997 <128421334+Paragon1997@users.noreply.github.com>
2023-04-21 08:23:44 +0200

    tqdm update

--- ASYMDED.py
+++ ASYMDED.py
@@ -32,7 +32,7 @@ if __name__ == '__main__':
             (nd[j], NewSigma, DOST[j], Lor, omega, selectpT, selectpcT),labelnames[j]=DEDlib.main(**input[i],posb=2),'$\\rho,\\Sigma_0=%.3f$'%input[i]['Sigma']
             DEDlib.DOSplot(DOST[j], Lor, omega,file+'%.16fSigma'%input[i]['Sigma'],'$\\rho,\\Sigma_0=%.3f$'%input[i]['Sigma'])
             DEDlib.textfileW(omega,np.ravel(selectpT),np.ravel(selectpcT),DOST[j],file+'%.16fSigma'%input[i]['Sigma'],NewSigma)
-            if np.isclose(input[i]['Sigma'],np.real(NewSigma[500]),rtol=1e-4): break
+            if np.isclose(input[i]['Sigma'],np.real(NewSigma[500]),rtol=1e-4, atol=1e-5): break
             input[i]['Sigma']=np.real(NewSigma[500])
         pbar.close()
         np.savetxt(file+'%.16fSigma'%np.real(NewSigma[500])+'nd.txt',nd,delimiter='\t', newline='\n')
