a0a21150e780057ac9defe10dfdc329258b0c4b2
Paragon1997 <128421334+Paragon1997@users.noreply.github.com>
2023-04-21 11:30:59 +0200

    Graph update

--- DEDliblog.py
+++ DEDliblog.py
@@ -1,5 +1,5 @@
 ## module DEDlib
-''' DEDlib is a Distributional Exact Diagonalization tooling library for study of Anderson (multi-)impurity model in Graphene Nanoribbons'''
+''' DEDliblog is a Distributional Exact Diagonalization tooling library for study of Anderson (multi-)impurity model in Graphene Nanoribbons'''
 
 import warnings
 warnings.filterwarnings("ignore", category=RuntimeWarning)
