f1e15c28273e786c81109b2c0630593ea9874be1
Paragon1997 <128421334+Paragon1997@users.noreply.github.com>
2023-04-21 08:24:26 +0200

    asym update

--- DED.py
+++ DED.py
@@ -112,10 +112,10 @@ if __name__ == '__main__':
             (nd[j], NewSigma, DOST[j], Lor, omega, selectpT, selectpcT),labelnames[j]=DEDlib.main(**input[i],posb=2),'$\\rho,\\Sigma_0=%.3f$'%input[i]['Sigma']
             DEDlib.DOSplot(DOST[j], Lor, omega,file+'%.16fSigma'%input[i]['Sigma'],'$\\rho,\\Sigma_0=%.3f$'%input[i]['Sigma'])
             DEDlib.textfileW(omega,np.ravel(selectpT),np.ravel(selectpcT),DOST[j],file+'%.16fSigma'%input[i]['Sigma'],NewSigma)
-            input[i]['Sigma']=np.real(NewSigma[500])
             if np.isclose(input[i]['Sigma'],np.real(NewSigma[500]),rtol=1e-4, atol=1e-5): break
+            input[i]['Sigma']=np.real(NewSigma[500])
         pbar.close()
-        np.savetxt(file+'%.16fSigma'%input[i]['Sigma']+'nd.txt',nd,delimiter='\t', newline='\n')
+        np.savetxt(file+'%.16fSigma'%np.real(NewSigma[500])+'nd.txt',nd,delimiter='\t', newline='\n')
         DEDlib.DOSmultiplot(omega,np.tile(omega, (j+1,1)),DOST[~np.all(DOST == 0, axis=1)],np.tile(len(omega), j+1),labelnames[:j+1].astype(str),'Asymtotal'+file,DEDlib.Lorentzian(omega,0.3,4,input[i]['Ed'],3/2)[0])
     filenames.close()
     
