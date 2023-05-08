cfe56261ea24fcbf9da027236a1bb818e36e9c91
Paragon1997 <128421334+Paragon1997@users.noreply.github.com>
2023-05-08 10:52:22 +0200

    STDED

--- DED.py
+++ DED.py
@@ -129,7 +129,7 @@ if __name__ == '__main__':
             DOST=np.zeros((len(filenames),4001),dtype = 'float')
             psi,SPG,eig,SPrho0=DEDlib.GrapheneAnalyzer(ip[j],DEDlib.Graphenecirclestruct(r,1),colorbnd[j],'GrapheneCirc'+str(r)+'r')
             for i,file in enumerate(filenames):
-                if j==1: inp[i]['Edcalc']='AS'
+                if j==1 or l==1: inp[i]['Edcalc']='AS'
                 else: inp[i]['Edcalc']=''
                 nd[j,i], AvgSigmadat, DOST[i], nonintrho, omega, selectpT, selectpcT,tsim=DEDlib.Graphene_main(psi,SPG,eig,SPrho0,**inp[i],posb=3)
                 DEDlib.DOSplot(DOST[i], nonintrho, omega,file+selecm[l],labelnames[i],log=True)
