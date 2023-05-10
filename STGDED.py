fb3fb6726f9733f4c82c2f942b9e1f3e8a327f3b
Paragon1997 <128421334+Paragon1997@users.noreply.github.com>
2023-05-10 15:14:58 +0200

    Execution

--- STGDED.py
+++ STGDED.py
@@ -19,7 +19,7 @@ import DEDlib as DEDlib
 
 if __name__ == '__main__':
     #Impurity entropy for different Gamma values of the SAIM
-    input={"N" : 20000, "poles" : 6, "ctype" : 'sn'}
+    input={"N" : 2000, "poles" : 6, "ctype" : 'sn'}
     Gamma,filenames,labelnames,S_imp,S_tot,S_bath=[0.2,0.3,0.5,0.9],tqdm(['ST0_2G','ST0_3G','ST0_5G','ST0_9G'],position=0,leave=False,desc='No. Gamma Entropy DED sims',bar_format='{l_bar}{bar:10}{r_bar}{bar:-10b}'),['$\Gamma\it{=0.2}$','$\Gamma\it{=0.3}$','$\Gamma\it{=0.5}$','$\Gamma\it{=0.9}$'],np.zeros((4,801)),np.zeros((4,801)),np.zeros((4,801))
     for i,file in enumerate(filenames):
         S_imp[i],S_tot[i],S_bath[i],Nfin,Tk,tsim=DEDlib.Entropyimp_main(**input,Gamma=Gamma[i])
