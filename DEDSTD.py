f5b0128c30c686d1db47f56af335fe252b0c3b41
Roel van Silfhout <rvsilfhout@qblox.com>
2023-05-27 13:42:12 +0200

    std

---
+++ DEDSTD.py
@@ -0,0 +1,21 @@
+import warnings
+warnings.filterwarnings("ignore", category=RuntimeWarning)
+from tqdm.auto import tqdm,trange
+import time
+from qutip import *
+import numpy as np
+import matplotlib.pyplot as plt
+import kwant
+import math
+from math import  sqrt
+import cmath
+from numpy.lib.scimath import sqrt as csqrt
+import scipy
+from scipy.optimize import fsolve
+import copy
+from itertools import repeat
+
+import DEDlib
+
+if __name__ == '__main__':
+    
