8ccaa69df5132b40ce1795f707bac109d1c1dce2
Paragon1997 <128421334+Paragon1997@users.noreply.github.com>
2023-06-01 13:19:37 +0200

    T eta

--- DEDlib.py
+++ DEDlib.py
@@ -113,9 +113,9 @@ Constraint implementation function for DED method with various possible constrai
         evals,evecs=scipy.linalg.eigh(H.data.toarray())
         if ctype=='ssn':
             Boltzmann=np.exp(-abs(evals[find_nearest(np.diag(np.conj(evecs).T@n[0].data@evecs),np.conj(vecs)@n[0].data@vecs.T)]-evals[0])/Tk)*Nfin.astype('int')
-            return MBGAIM(omega,H,c,eta,Tk,Boltzmann,evals,evecs,1e-4,np.array([-2,-1,0,1,2])),True
+            return MBGAIM(omega,H,c,eta,Tk,Boltzmann,evals,evecs,4e-4,np.array([-2,-1,0,1,2])),True
         else:
-            return MBGAIM(omega,H,c,eta,Tk,np.exp(-abs(evals[find_nearest(np.diag(np.conj(evecs).T@n[0].data@evecs),np.conj(vecs)@n[0].data@vecs.T)]-evals[0])/Tk),evals,evecs,1e-4,np.array([-2,-1,0,1,2])),True
+            return MBGAIM(omega,H,c,eta,Tk,np.exp(-abs(evals[find_nearest(np.diag(np.conj(evecs).T@n[0].data@evecs),np.conj(vecs)@n[0].data@vecs.T)]-evals[0])/Tk),evals,evecs,4e-4,np.array([-2,-1,0,1,2])),True
     elif ctype[0]=='n':
         vecs=scipy.sparse.csr_matrix(np.vstack((scipy.sparse.linalg.eigsh(np.real(H0.data),k=1,which='SA')[1][:,0],
                                                 scipy.sparse.linalg.eigsh(np.real(H.data),k=1,which='SA')[1][:,0])))
