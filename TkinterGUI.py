e87b3c3078f6daaa47ae4839aa7728f3845987c1
Paragon1997 <128421334+Paragon1997@users.noreply.github.com>
2024-03-09 10:03:28 +0100

    reset and explorer

--- TkinterGUI.py
+++ TkinterGUI.py
@@ -47,12 +47,13 @@ Writes ``.json`` file including DED simulation settings and collected data."""
     jsonObj=json.dumps(data,cls=NumpyArrayEncoder)
     with open(name,"w") as outfile: outfile.write(jsonObj)
 
-def savfilename(entry):
-    """``savfilename(entry)``.\n
+def savfilename(root,entry):
+    """``savfilename(root,entry)``.\n
 Checks whether filename input in entry is valid for use in constructing ``.json`` file."""
     if not entry.get().endswith(".json"):
         entry.delete(0,last_index=tk.END)
         entry.insert(0,'Try again')
+    _=savedata(root,entry)
 
 def savedata(root,entry):
     """``savedata(root,entry)``.\n
@@ -152,7 +153,7 @@ class SAIMWINDOW(ctk.CTkToplevel):
 Class for Symmetric Anderson impurity model DED simmulation window."""
     def __init__(self,selfroot,N=200000,poles=4,U=3,Sigma=3/2,Ed=-3/2,Gamma=0.3,SizeO=1001,etaco=[0.02,1e-39],ctype='n',Edcalc='',bound=3,Tk=[0],Nimpurities=1,U2=0,J=0,posb=1,log=False,base=1.5,ymax=1.2,logy=False,fDOScolor='b',*args,**kwargs):
         super().__init__(*args,**kwargs)
-        self.root,self.paused,self.started,self.stopped,self.loaded,self.poleDOS,self.telapsed,self.DEDargs=selfroot,False,False,False,False,False,0,[N,poles,U,Sigma,Ed,Gamma,ctype,Edcalc,Nimpurities,U2,J,Tk,etaco,SizeO,bound,posb,log,base,ymax,logy,fDOScolor]
+        self.root,self.paused,self.started,self.stopped,self.loaded,self.parainitialized,self.poleDOS,self.telapsed,self.DEDargs=selfroot,False,False,False,False,False,False,0,[N,poles,U,Sigma,Ed,Gamma,ctype,Edcalc,Nimpurities,U2,J,Tk,etaco,SizeO,bound,posb,log,base,ymax,logy,fDOScolor]
         if self.DEDargs[16]:self.omega,self.Npoles=np.concatenate((-np.logspace(np.log(self.DEDargs[14])/np.log(self.DEDargs[17]),np.log(1e-5)/np.log(self.DEDargs[17]),int(np.round(self.DEDargs[13]/2)),base=self.DEDargs[17]),np.logspace(np.log(1e-5)/np.log(self.DEDargs[17]),np.log(self.DEDargs[14])/np.log(self.DEDargs[17]),int(np.round(self.DEDargs[13]/2)),base=self.DEDargs[17]))),int(self.DEDargs[1]/self.DEDargs[8])
         else:self.omega,self.Npoles=np.linspace(-self.DEDargs[14],self.DEDargs[14],self.DEDargs[13]),int(self.DEDargs[1]/self.DEDargs[8])
         self.c,self.pbar,self.eta=[Jordan_wigner_transform(i,2*self.DEDargs[1]) for i in range(2*self.DEDargs[1])],trange(self.DEDargs[0],position=self.DEDargs[15],leave=False,desc='Iterations',bar_format='{l_bar}{bar:10}{r_bar}{bar:-10b}'),self.DEDargs[12][0]*abs(self.omega)+self.DEDargs[12][1]
@@ -164,7 +165,7 @@ Class for Symmetric Anderson impurity model DED simmulation window."""
         self.resizable(width=False,height=False)
         self.after(200,self.focus)
         self.grid_columnconfigure(1,weight=1)
-        self.grid_columnconfigure((2,3),weight=0)
+        self.grid_columnconfigure((2,3,4),weight=0)
         self.grid_rowconfigure((0,1,2),weight=1)
         self.sidebar_frame=ctk.CTkFrame(self,width=140,corner_radius=0)
         self.sidebar_frame.grid(row=0,column=0,rowspan=5,sticky="nsew")
@@ -209,34 +210,41 @@ Class for Symmetric Anderson impurity model DED simmulation window."""
         self.scaling_optionemenu.set(str(self.DEDargs[1]))
         self.Tk_Entry.insert(0,str(self.DEDargs[11]))
         self.entry=ctk.CTkEntry(self,placeholder_text="C:\\")
-        self.entry.grid(row=3,column=1,columnspan=2,padx=(20,0),pady=(20,0),sticky="nsew")
-        self.main_button=ctk.CTkButton(self,text="Submit file to load",fg_color="transparent",width=160,border_width=2,text_color=("gray10","#DCE4EE"),command=self.fileloader)
-        self.main_button.grid(row=3,column=3,padx=(20,20),pady=(20,0),sticky='w')
+        self.entry.grid(row=3,column=1,columnspan=2,padx=(20,0),pady=(10,5),sticky="nsew")
+        self.openfile_button=ctk.CTkButton(self,text="Open file",fg_color="transparent",width=100,border_width=2,text_color=("gray10","#DCE4EE"),command=self.openfile)
+        self.openfile_button.grid(row=3,column=3,padx=(20,5),pady=(10,5),sticky="w")
+        self.main_button=ctk.CTkButton(self,text="Submit file to load",fg_color="transparent",width=150,border_width=2,text_color=("gray10","#DCE4EE"),command=self.fileloader)
+        self.main_button.grid(row=3,column=4,padx=(0,20),pady=(10,5),sticky="e")
         self.entry_2=ctk.CTkEntry(self,placeholder_text="example.json")
-        self.entry_2.grid(row=4,column=1,columnspan=2,padx=(20,0),pady=10,sticky="nsew")
-        self.main_button_2=ctk.CTkButton(self,text="Submit save location",fg_color="transparent",width=160,border_width=2,text_color=("gray10","#DCE4EE"),command=lambda:savfilename(self.entry_2))
-        self.main_button_2.grid(row=4,column=3,padx=(20,20),pady=10,sticky='w')
+        self.entry_2.grid(row=4,column=1,columnspan=2,padx=(20,0),pady=(5,10),sticky="nsew")
+        self.saveasfile_button=ctk.CTkButton(self,text="Save as file",fg_color="transparent",width=100,border_width=2,text_color=("gray10","#DCE4EE"),command=self.saveasfile)
+        self.saveasfile_button.grid(row=4,column=3,padx=(20,5),pady=(5,10),sticky="w")
+        self.main_button_2=ctk.CTkButton(self,text="Submit file to save",fg_color="transparent",width=150,border_width=2,text_color=("gray10","#DCE4EE"),command=lambda:savfilename(self,self.entry_2))
+        self.main_button_2.grid(row=4,column=4,padx=(0,20),pady=(5,10),sticky='e')
         self.plot_frame=ctk.CTkFrame(self,width=250,height=380)
         self.plot_frame.grid(row=0,column=1,columnspan=2,rowspan=1,padx=(20,0),pady=(20,10),sticky="nsew")
         self.slider_progressbar_frame=ctk.CTkFrame(self)
         self.slider_progressbar_frame.grid(row=1,column=1,columnspan=2,padx=(20,0),pady=(5,0),sticky="nsew")
-        self.slider_progressbar_frame.grid_columnconfigure((0,1,2,3,4),weight=1)
+        self.slider_progressbar_frame.grid_columnconfigure((0,1,2,3,4,5),weight=1)
         self.progressbar_1=ProgressBar(master=self.slider_progressbar_frame,variable=ctk.IntVar(value=0),itnum=self.pbar.n,Total=self.pbar.total,height=30)
-        self.progressbar_1.grid(row=0,column=0,columnspan=5,padx=20,pady=(5,0),sticky="nsew")
+        self.progressbar_1.grid(row=0,column=0,columnspan=6,padx=20,pady=(5,0),sticky="nsew")
         self.start_button=ctk.CTkButton(self.slider_progressbar_frame,text="Start",command=self.startDED)
-        self.start_button.grid(row=1,column=0,padx=5,pady=(5,10))
+        self.start_button.grid(row=1,column=0,padx=4,pady=(5,10))
         self.pause_button=ctk.CTkButton(self.slider_progressbar_frame,text="Pause",command=self.pauseDED)
-        self.pause_button.grid(row=1,column=1,padx=5,pady=(5,10))
+        self.pause_button.grid(row=1,column=1,padx=4,pady=(5,10))
         self.stop_button=ctk.CTkButton(self.slider_progressbar_frame,text="Stop",command=self.stopDED)
-        self.stop_button.grid(row=1,column=2,padx=5,pady=(5,10))
-        self.show_button=ctk.CTkButton(self.slider_progressbar_frame,text="Show Results",command=self.showgraph)
-        self.show_button.grid(row=1,column=3,padx=5,pady=(5,10))
+        self.stop_button.grid(row=1,column=2,padx=4,pady=(5,10))
+        self.show_button=ctk.CTkButton(self.slider_progressbar_frame,text="Show Graph",command=self.showgraph)
+        self.show_button.grid(row=1,column=3,padx=4,pady=(5,10))
         self.save_button=ctk.CTkButton(self.slider_progressbar_frame,text="Save Graph",command=lambda:savegraph(self,self.entry_2))
-        self.save_button.grid(row=1,column=4,padx=5,pady=(5,10))
+        self.save_button.grid(row=1,column=4,padx=4,pady=(5,10))
+        self.reset_button=ctk.CTkButton(self.slider_progressbar_frame,text="Reset",command=self.resetDED)
+        self.reset_button.grid(row=1,column=5,padx=4,pady=(5,10))
         self.pause_button.configure(state="disabled")
         self.stop_button.configure(state="disabled")
+        self.reset_button.configure(state="disabled")
         self.settings_tab=ctk.CTkTabview(self, width=261)
-        self.settings_tab.grid(row=0,column=3,rowspan=2,padx=(20,20),pady=(20,0),sticky="nsew")
+        self.settings_tab.grid(row=0,column=3,rowspan=2,columnspan=2,padx=(20,20),pady=(20,0),sticky="nsew")
         self.settings_tab.add("Adv.")
         self.settings_tab.add("Multi orb.")
         self.settings_tab.add("Graph")
@@ -335,7 +343,7 @@ Class for Symmetric Anderson impurity model DED simmulation window."""
                 else:
                     self.N_Entry.delete(0,last_index=tk.END)
                     self.N_Entry.insert(0,str(self.pbar.n))
-                self.DEDargs[1:],self.progressbar_1.Total,self.progressbar_1.itnum=[int(self.scaling_optionemenu.get()),float(self.U_Entry.get()),float(self.Sigma_Entry.get()),float(self.Ed_Entry.get()),float(self.Gamma_Entry.get()),self.ctype_optionemenu.get(),self.Edcalc_optionemenu.get(),int(self.Nimpurities_optionemenu.get()),float(self.U2_Entry.get()),float(self.J_Entry.get()),literal_eval(self.Tk_Entry.get()),literal_eval(self.eta_Entry.get()),int(self.SizeO_Entry.get()),float(self.bound_Entry.get()),self.DEDargs[15],bool(self.log_checkbox.get()),float(self.base_Entry.get()),float(self.graphymax_Entry.get()),bool(self.graphlogy_checkbox.get()),self.graphfDOScolor_Entry.get()],self.DEDargs[0],self.pbar.n
+                self.parainitialized,self.DEDargs[1:],self.progressbar_1.Total,self.progressbar_1.itnum=True,[int(self.scaling_optionemenu.get()),float(self.U_Entry.get()),float(self.Sigma_Entry.get()),float(self.Ed_Entry.get()),float(self.Gamma_Entry.get()),self.ctype_optionemenu.get(),self.Edcalc_optionemenu.get(),int(self.Nimpurities_optionemenu.get()),float(self.U2_Entry.get()),float(self.J_Entry.get()),literal_eval(self.Tk_Entry.get()),literal_eval(self.eta_Entry.get()),int(self.SizeO_Entry.get()),float(self.bound_Entry.get()),self.DEDargs[15],bool(self.log_checkbox.get()),float(self.base_Entry.get()),float(self.graphymax_Entry.get()),bool(self.graphlogy_checkbox.get()),self.graphfDOScolor_Entry.get()],self.DEDargs[0],self.pbar.n
                 self.progressbar_1.set(self.pbar.n/self.DEDargs[0])
                 self.U_Entry.configure(state="disabled")
                 self.Sigma_Entry.configure(state="disabled")
@@ -360,6 +368,26 @@ Class for Symmetric Anderson impurity model DED simmulation window."""
                     self.eta,self.AvgSigmadat,self.Nfin,self.nd,self.Npoles,self.c,self.Lor=self.DEDargs[12][0]*abs(self.omega)+self.DEDargs[12][1],np.zeros((len(self.DEDargs[11]),self.DEDargs[13]),dtype='complex_'),np.zeros(len(self.DEDargs[11]),dtype='float'),np.zeros(len(self.DEDargs[11]),dtype='complex_'),int(self.DEDargs[1]/self.DEDargs[8]),[Jordan_wigner_transform(i,2*self.DEDargs[1]) for i in range(2*self.DEDargs[1])],Lorentzian(self.omega,self.DEDargs[5],self.DEDargs[1],self.DEDargs[4],self.DEDargs[3])[0]
                     (self.Hn,self.n)=Operators(self.c,self.DEDargs[8],self.DEDargs[1])
             except: pass
+
+    def saveasfile(self):
+        """``saveasfile(self)``.\n
+    Class method to choose save file location for a DED data file in File Explorer."""
+        self.savefile=ctk.filedialog.asksaveasfilename(initialdir="",title="Save DED Data JSON File as",filetypes=[('JSON files','*.json')])
+        self.root.lower()
+        if self.savefile:
+            self.entry_2.delete(0,last_index=tk.END)
+            if not self.savefile.endswith(".json"): self.savefile+=".json"
+            self.entry_2.insert(0,self.savefile)
+            _=savedata(self,self.entry_2)
+
+    def openfile(self):
+        """``openfile(self)``.\n
+    Class method to choose a DED data file to load in File Explorer."""
+        self.loadfile=ctk.filedialog.askopenfilename(initialdir="",title="Select a DED Data JSON File",filetypes=[('JSON files','*.json')])
+        self.root.lower()
+        if self.loadfile:
+            self.entry.delete(0,last_index=tk.END)
+            self.entry.insert(0,self.loadfile)
 
     def fileloader(self):
         """``fileloader(self)``.\n
@@ -431,29 +459,15 @@ Class for Symmetric Anderson impurity model DED simmulation window."""
     def startDED(self):
         """``startDED(self)``.\n
     Class method to start DED calculations by initiating loop."""
-        self.start_button.configure(state="disabled")
-        self.started,self.stopped,self.pbar.start_t=True,False,self.pbar._time()-self.telapsed
-        self.N_Entry.configure(state="disabled")
-        self.U_Entry.configure(state="disabled")
-        self.Sigma_Entry.configure(state="disabled")
-        self.Ed_Entry.configure(state="disabled")
-        self.Gamma_Entry.configure(state="disabled")
-        self.scaling_optionemenu.configure(state="disabled")
-        self.Tk_Entry.configure(state="disabled")
-        self.sidebar_button_3.configure(state="disabled")
-        self.eta_Entry.configure(state="disabled")
-        self.SizeO_Entry.configure(state="disabled")
-        self.bound_Entry.configure(state="disabled")
-        self.ctype_optionemenu.configure(state="disabled")
-        self.log_checkbox.configure(state="disabled")
-        self.base_Entry.configure(state="disabled")
-        self.Edcalc_optionemenu.configure(state="disabled")
-        self.Nimpurities_optionemenu.configure(state="disabled")
-        self.U2_Entry.configure(state="disabled")
-        self.J_Entry.configure(state="disabled")
-        self.loopDED()
-        self.pause_button.configure(state="normal")
-        self.stop_button.configure(state="normal")
+        if not self.parainitialized: self.parainit()
+        if self.parainitialized:
+            self.start_button.configure(state="disabled")
+            self.started,self.stopped,self.pbar.start_t=True,False,self.pbar._time()-self.telapsed
+            self.loopDED()
+            self.pause_button.configure(state="normal")
+            self.stop_button.configure(state="normal")
+            self.reset_button.configure(state="normal")
+            self.main_button.configure(state="disabled")
 
     def loopDED(self):
         """``loopDED(self)``.\n
@@ -474,6 +488,7 @@ Class for Symmetric Anderson impurity model DED simmulation window."""
             self.stopDED()
             self.showgraph()
             savegraph(self,self.entry_2)
+        elif not self.parainitialized: self.stopped=False
 
     def iterationDED(self,reset=False):
         """``iterationDED(self,reset=False)``.\n
@@ -501,8 +516,8 @@ Class for Symmetric Anderson impurity model DED simmulation window."""
             self.stopped,self.paused,self.started=True,False,False
             self.pause_button.configure(state="disabled")
             self.stop_button.configure(state="disabled")
-            if self.pbar.n!=self.DEDargs[0]:
-                self.start_button.configure(state="normal")
+            if self.pbar.n!=self.DEDargs[0]: self.start_button.configure(state="normal")
+        elif not self.stopped and not self.paused: self.pauseDED()
 
     def showgraph(self):
         """``showgraph(self)``.\n
@@ -542,6 +557,40 @@ Class for Symmetric Anderson impurity model DED simmulation window."""
             self.plot_frame.update()
         except: pass
         plt.close()
+
+    def resetDED(self):
+        self.pbar.reset()
+        if self.loaded:
+            self.progressbar_1.itnum=self.pbar.n=self.data["Nit"]
+            self.Nfin,self.AvgSigmadat,self.nd,self.telapsed=np.array(self.data["Nfin"]),np.array(self.data["AvgSigmadat"]*self.data["Nfin"]).squeeze(),np.array(np.array(self.data["nd"],dtype=np.complex128)*np.array(self.data["Nfin"],dtype=np.float64),dtype=np.complex128),self.data["telapsed"]
+        else: 
+            self.progressbar_1.itnum=self.pbar.n=0
+            self.AvgSigmadat,self.Nfin,self.nd,self.telapsed=np.zeros((len(self.DEDargs[11]),self.DEDargs[13]),dtype='complex_'),np.zeros(len(self.DEDargs[11]),dtype='float'),np.zeros(len(self.DEDargs[11]),dtype='complex_'),0
+        self.progressbar_1.set(self.pbar.n/self.DEDargs[0])
+        self.pbar.refresh()
+        self.paused,self.started,self.stopped,self.loaded,self.parainitialized=False,False,True,False,False
+        self.start_button.configure(state="normal")
+        self.pause_button.configure(state="disabled")
+        self.stop_button.configure(state="disabled")
+        self.reset_button.configure(state="disabled")
+        self.main_button.configure(state="normal")
+        self.N_Entry.configure(state="normal")
+        self.U_Entry.configure(state="normal")
+        self.Sigma_Entry.configure(state="normal")
+        self.Ed_Entry.configure(state="normal")
+        self.Gamma_Entry.configure(state="normal")
+        self.scaling_optionemenu.configure(state="normal")
+        self.Tk_Entry.configure(state="normal")
+        self.eta_Entry.configure(state="normal")
+        self.SizeO_Entry.configure(state="normal")
+        self.bound_Entry.configure(state="normal")
+        self.ctype_optionemenu.configure(state="normal")
+        self.log_checkbox.configure(state="normal")
+        self.base_Entry.configure(state="normal")
+        self.Edcalc_optionemenu.configure(state="normal")
+        self.Nimpurities_optionemenu.configure(state="normal")
+        self.U2_Entry.configure(state="normal")
+        self.J_Entry.configure(state="normal")
 
 class polesWINDOW(ctk.CTkToplevel):
     def __init__(self,selfroot,N=200000,poles=4,U=3,Sigma=3/2,Ed=-3/2,Gamma=0.3,SizeO=1001,etaco=[0.02,1e-39],ctype='n',Edcalc='',bound=3,Tk=[0],Nimpurities=1,U2=0,J=0,posb=1,log=False,base=1.5,*args,**kwargs):
